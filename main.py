b6b6b00f1b8f5e82cdc0c689b3c5f72a3ee807fb
Rene Langer <renelanger96@live.de>
2018-04-12 07:53:39 +0200

    Added status LED support and battery handler

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import servo
 import server
+import RPi.GPIO as GPIO
 
 import time
 
@@ -9,26 +10,32 @@ if __name__ == '__main__':
     DEBUG = False
 
     start_time = time.time()
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(21, GPIO.OUT)
+    led = GPIO.PWM(21, 2)
+
+    led.start(0.5)
 
     print("##########################################################\n"
           "### RC-Car\n"
           "### Server Version 0.7.1b - Expecting client version 0.7b\n"
           "### Server is going to be hosted on {}\n".format(IP_ADDRESS) +
-          "### \n")
+          "###")
 
     servo_control = servo.PwmServo()
     server = server.Server(IP_ADDRESS, 9999)
 
-    print("### Initialization finished in {:1.4f} seconds\n".format((time.time()-start_time)))
+    print("### Initialization finished in {:1.4f} seconds".format((time.time()-start_time)))
     print("##########################################################\n\n")
     while not server.termination_pending:
         print("\tWaiting for incoming connection...")
+        led.ChangeDutyCycle(1)
 
         # wait until a connection is established
         server.await_connection()
 
         while server.connection_active:
-
+            led.ChangeDutyCycle(0)
             data_type = server.receive()
 
             if len(data_type) == 1 and data_type == server.SERVER_REQUEST:
@@ -61,6 +68,10 @@ if __name__ == '__main__':
                     # send done code 0x12
                     print("\t\tData: 0x{0:x};0x{1:x};0x{2:x}\r".format(data[0], data[1], data[2]), end="", flush=True)
                     server.send_byte(server.SERVER_FINISHED)
+
+            elif len(data_type) == 1 and data_type == server.SERVER_BATTERY_REQUEST:
+                # put battery request handler here
+                pass
 
             else:
                 print("Unexpected Type:{} \r".format(data_type))
